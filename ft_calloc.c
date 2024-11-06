022da7e2d5791751a984cc18ecc5016ff78c353d
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-11-06 23:24:06 +0100

    fix(libc): reimplement ft_calloc with overflow check and ft_bzero reuse
    Rewrote ft_calloc to handle overflow issues and reused ft_bzero for efficiency.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,26 +6,19 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 19:23:51 by alvinram          #+#    #+#             */
-/*   Updated: 2024/10/30 01:31:37 by alvinram         ###   ########.fr       */
+/*   Updated: 2024/11/06 23:21:29 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t number, size_t size)
+void *ft_calloc(size_t count, size_t size)
 {
-	char	*allocated_memory;
-	size_t	byte_index;
+	void *pointer;
 
-	if (number != 0 && size > SIZE_MAX / number)
-	{
-		return (NULL);
-	}
-	allocated_memory = malloc(number * size);
-	if (!allocated_memory)
-		return (NULL);
-	byte_index = -1;
-	while (++byte_index < size * number)
-		allocated_memory[byte_index] = 0;
-	return (allocated_memory);
+	pointer = malloc(count * size);
+	if (pointer == NULL)
+		return (pointer);
+	ft_bzero(pointer, size * count);
+	return (pointer);
 }
