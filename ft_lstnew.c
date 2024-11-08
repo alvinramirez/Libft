d9eebf06f52509f72ab1cb15e133c463b8f5567c
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-11-08 22:34:02 +0100

    feat(libc): add ft_lstnew function for linked list node creation
    Added ft_lstnew.c to create new linked list nodes with generic pointer support, allowing flexible data storage in the list structure.

---
+++ ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 22:27:31 by alvinram          #+#    #+#             */
+/*   Updated: 2024/11/08 22:27:59 by alvinram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list *new;
+
+	if (!(new = (t_list *)malloc(sizeof(t_list))))
+		return (0);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
