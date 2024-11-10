069811bd8a0160b8f02b803ac893ff0515d02d07
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-11-10 23:15:50 +0100

    refactor(libc): optimize ft_strrchr for improved efficiency
    Refactored ft_strrchr to reduce code length and enhance performance while maintaining original functionality.

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 01:22:16 by alvinram          #+#    #+#             */
-/*   Updated: 2024/10/30 01:43:20 by alvinram         ###   ########.fr       */
+/*   Updated: 2024/11/10 21:45:38 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,15 @@
 char	*ft_strrchr(const char *string, int character)
 {
 	char	*last_found_position;
-	char	*current_position;
 
-	last_found_position = 0;
-	current_position = (char *)string;
-	current_position = ft_strchr(current_position, character);
-	while (current_position != 0)
+	last_found_position = (NULL);
+	while (*string)
 	{
-		last_found_position = current_position;
-		current_position++;
-		current_position = ft_strchr(current_position, character);
+		if (*string == (char)character)
+			last_found_position = (char *)string;
+		string++;
 	}
+	if ((char)character == '\0')
+		return ((char *)string);
 	return (last_found_position);
 }
