a4a9f70db964852de59f371b2d2307717213d104
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-11-12 17:40:56 +0100

    style(libc): adjust ft_calloc formatting for Norminette compliance
    Updated tabulations and spacing in ft_calloc.c to align with Norminette style requirements.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,15 +6,15 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 19:23:51 by alvinram          #+#    #+#             */
-/*   Updated: 2024/11/06 23:21:29 by alvinram         ###   ########.fr       */
+/*   Updated: 2024/11/11 09:46:19 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
-	void *pointer;
+	void	*pointer;
 
 	pointer = malloc(count * size);
 	if (pointer == NULL)
