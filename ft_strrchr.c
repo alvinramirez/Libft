725e0ce4f15af328e5e5af39d8314e6fc986d8e1
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-11-12 18:20:07 +0100

    refactor(libc): simplify and enhance ft_strrchr for efficiency and clarity
    Reimplemented ft_strrchr starting from the null character, making the function simpler, more robust, and efficient while maintaining its intended functionality.

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 01:22:16 by alvinram          #+#    #+#             */
-/*   Updated: 2024/11/10 21:45:38 by alvinram         ###   ########.fr       */
+/*   Updated: 2024/11/11 09:52:28 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,14 @@
 
 char	*ft_strrchr(const char *string, int character)
 {
-	char	*last_found_position;
+	int	index;
 
-	last_found_position = (NULL);
-	while (*string)
+	index = ft_strlen(string);
+	while (index >= 0)
 	{
-		if (*string == (char)character)
-			last_found_position = (char *)string;
-		string++;
+		if (string[index] == (char)character)
+			return ((char *)&string[index]);
+		index--;
 	}
-	if ((char)character == '\0')
-		return ((char *)string);
-	return (last_found_position);
+	return (NULL);
 }
